cfb76beacbdfd7fad8085027621b81d458c7ec99
Ugurcan06 <ugurcan_gurel@hotmail.com>
2023-04-22 18:02:45 +0300

    SpotifyWebPage

---
+++ app.js
@@ -0,0 +1,17 @@
+$('.owl-carousel ').owlCarousel  ({
+    loop:false,
+    nav:false,
+   responsiveClass:true,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
